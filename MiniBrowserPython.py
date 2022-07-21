7ea5378e69c4222d0e6f9fcae4d71e3a8a36f70e
Dalison Dieyson Rocha <dalisonfatality@gmail.com>
2022-07-21 14:19:26 -0300

    Atualização MiniBrowser
    tratamento de conexão

--- MiniBrowserPython.py
+++ MiniBrowserPython.py
@@ -1,52 +1,65 @@
+# coding utf-8
+# MiniBrowser.py
+
 import os
 import sys
-import socket 
-from urllib import parse 
+import socket
+from urllib import parse
 import tempfile
 import webbrowser
 
-#serverName = "www.google.com"
+# Requisitando link do servidor e porta
 serverName = input("Entre com o endereço do servidor HTTP: ")
 serverPort = int(input("Entre com o número da porta: "))
-#serverPort = 80
+# Definindo tipo de socket no caso IPV4
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect((serverName, serverPort)) #conectando ao socket
 
-print("\n Nome introduzido no servidor: ", serverName)
+# conectando ao socket
+try:
+    clientSocket.connect((serverName, serverPort))
+    print("#conectando ao servior")
+except:
+    print("#Erro ao conectar  \n..Fechando..")
+    sys.exit()
 
+print("\n#Requisitando no servidor: ", serverName)
 request = "GET / HTTP/1.1\r\nHost: " + serverName + "\r\nConnection: close \r\n\r\n"
+
+print("#Enviando requisição")
 clientSocket.sendall(request.encode('utf-8'))
 
-#comecando a trabalhar com o arquivo que vai receber o html
-f = open("index.html", "bw") #limpando se já existir
-f.close() 
-f = open("index.html", "br+") #vai receber o HTML sem HTTP header
+# comecando a trabalhar com o arquivo que vai receber o html
+f = open("index.html", "bw")  # limpando se já existir
+f.close()
+f = open("index.html", "br+")  # vai receber o HTML sem HTTP header
 
-body = b"" #vai armazenar o recebido do servidor de socket
+body = b""  # vai armazenar o recebido do servidor de socket
 
+# salvando dados da request na variável body
 
-#salvando dados da request na variável body
-
+print("#--->salvando requisição<---")
 while True:
     peca = clientSocket.recv(4096)
-    body = body + peca;
-    if len(peca) < 1:     
+    body = body + peca
+    if len(peca) < 1:
         break
 
-#retirando o header da variável body para escrita
-headers =  body.split(b'\r\n\r\n')[0]
-body = body[len(headers)+4:]
+# retirando o header da variável body para escrita
+headers = body.split(b'\r\n\r\n')[0]
+body = body[len(headers) + 4:]
 
-#escrevendo no arquivo
+# escrevendo no arquivo
 f.write(body)
 
-#output: HTML
+# output: HTML no terminal
 print(body)
 
 body = body.decode('ISO-8859-1')
+# passando o caminho do arquivo html salvo para ser aberto no browser
 url = 'file://' + os.path.realpath('index.html')
 
-webbrowser.open(url,new=1) #abrindo o código html
+# Renderizando o arquivo com o módulo webbrowser
+webbrowser.open(url, new=1)  # abrindo o código html
 
 f.close()
 clientSocket.close()
